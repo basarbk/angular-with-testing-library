286e9b006a46c98db42964cb8a8c164c2096cb53
Basar Buyukkahraman <basarbk@yahoo.com>
2022-04-17 15:02:48 +0300

    Add test for heading

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { render, screen } from '@testing-library/angular';
 
 describe('AppComponent', () => {
   describe('Default test setup', () => {
@@ -29,5 +30,20 @@ describe('AppComponent', () => {
       const compiled = fixture.nativeElement as HTMLElement;
       expect(compiled.querySelector('.content span')?.textContent).toContain('testing-lib app is running!');
     });
+
+    it('should have Resources header', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('h2')?.textContent).toContain('Resources');
+    })
   })
+
+  describe('With testing-library', () => {
+    it('should have Resources header', async () => {
+      await render(AppComponent);
+      const header = screen.getByRole('heading', { name: 'Resources'});
+      expect(header).toBeTruthy();
+    })
+  });
 });
